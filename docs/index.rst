e464afe1b6252433c13dddd85a53c0e430298e0f
paul brian <paul@mikadosoftware.com>
2016-01-15 18:54:21 +0000

    Converted to run automatically from python.

--- docs/index.rst
+++ docs/index.rst
@@ -1,33 +1,45 @@
 HoloDeck
 ========
 
-Holodeck is an attempt to write a complete build server for Python.
+Holodeck is an attempt to write an immutable server build chain
+for Python packages.
 
-Using salt, we shall bring up and configure a build-server, install a venv,
-install a python package out of git, and wrap the whole venv up into a .deb file.
+At the moment I have a means to build .deb packages from a
+python package (say hosted on github, with a working `setup.py` file)
 
-This .deb file can then be taken to another server, built in the cloud using salt also, 
-and installed. That way we can build our version of a package once, and move it from test 
-to production, confident we are using the same code, same binary on live as we tested.
+The build can occur on a spun up cloud server thus meaning the build
+willcorrectly target the final destination OS, no matter what your
+laptop runs.
+
+Then we can spin up a destination cloud server and using saltstack / ansible (in transition) we can deploy the package, and configure it using holo-config)
+
+I have introduced a Docker build after this, so the final artifact can be either .deb or a docker image.   
+
+This .deb file can then be taken to another server, built in the cloud
+using salt also, and installed. That way we can build our version of a
+package once, and move it from test to production, confident we are
+using the same code, same binary on live as we tested.
 
 
 Holodeck is based on the `rant
-<http://hynek.me/articles/python-app-deployment-with-native-packages/>`_ of
-Python Core Committer Hynek Schlawack.
+<http://hynek.me/articles/python-app-deployment-with-native-packages/>`_
+of Python Core Committer Hynek Schlawack.
 
-It also owes a lot to `parcel <parcel.readthedocs.org>`_ - not necessarily that
-any of the codebase is the same but for the sheer get on and do it.  Sadly, I
-could not muster the energy to cross the hg/git divide so instead of
-contributing patches I simply redid.  They have better looking docs too.
+It also owes a lot to `parcel <parcel.readthedocs.org>`_ - not
+necessarily that any of the codebase is the same but for the sheer get
+on and do it.  Sadly, I could not muster the energy to cross the
+hg/git divide so instead of contributing patches I simply redid.  They
+have better looking docs too.
 
-HoloDeck is an attempt at a pun - the core idea is to wrap up an entire
-virtualenv and pass it from host to host.  In other words we `enclose virtual
-environments.` I never said it was a good pun.
+HoloDeck is an attempt at a pun - the core idea is to wrap up an
+entire virtualenv and pass it from host to host.  In other words we
+`enclose virtual environments.` I never said it was a good pun.
 
-The idea is to build a new wrapped venv for every commit, and install it onto 
-immutable servers as it progresses through testing.
+The idea is to build a new wrapped venv for every commit, and install
+it onto immutable servers as it progresses through testing.
 
-I hope this will facilitate more Python (web) packages in the `micro-services` style.
+I hope this will facilitate more Python (web) packages in the
+`micro-services` style.
 
 
 
--- holo/maker.py
+++ holo/maker.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-'''
-Create .deb files from python venvs as artifacts for deployment
+'''Create .deb files from python venvs as artifacts for deployment
 
-app_path is where we create the virtual env
-and it is also the destination for the final target venv.
-We cannot avoid this - .deb creates it from dirs
+app_path is where we create the virtual env and it is also the
+destination for the final target venv.  We cannot avoid this - .deb
+creates it from dirs
 
 todo: convert to run autmaotically within python (not pront cmds)
 todo: discvover postinst files and add as cmd switches to fpm
@@ -22,9 +21,33 @@ import pprint
 def gitfetch(url, parentfolder):
     '''Given a git url, retrieve to `parentfolder` 
     '''
+class SubCmd(object):
+    '''Smoothly act as store of a subprocess cmd
+
+    we want to have same command as a list for non-shell 
+    and in friendly form.
+
+    nb Its a lot easier to .join a list than parse a string
+    '''
+    def __init__(self, cmdlist, pythonstmt=None, args=None):
+        self.cmdlist = cmdlist
+        self.pythonstmt = pythonstmt
+        self.args = args
+            
+    def __repr__(self):
+        return " ".join(self.cmdlist)
     
+        
 
 class Deployment(object):
+
+    '''A big wrapper around different stages in making the 
+       python package into a .deb
+
+    '''
+    #: the root where the final .deb installed code will get put
+    #: it is also, for ease of building .debs, where we put the code
+    #: so the .deb making stage can find it
     BASE_PATH = '/mikado'
 
     def __init__(self, app_name, giturl):
@@ -39,33 +62,49 @@ class Deployment(object):
         self.pip_exe = os.path.join(self.venv_path, 'bin/pip')
         self.giturl = giturl
         self.cmds = []
-        
+
+
     #for now just build bash commands for later
     def prepare_venv(self):
-        self.cmds.append('rm -rf {}'.format(self.venv_path))
-        self.cmds.append('mkdir -p {}'.format(self.venv_path))
-        self.cmds.append('mkdir -p {}'.format(self.src_path))        
-        #chown
-        self.cmds.append('virtualenv -p {} {}'.format(
-                                               '/usr/bin/python',
-                                               self.venv_path)
-                        )
-        #replace with with
-        self.cmds.append('. {}/bin/activate'.format(self.venv_path))
+        #: build list of cmds to run.
+        for cmd in (
+                SubCmd(['rm', '-rf', self.venv_path]),
         
-        self.cmds.append('git clone {} {}'.format(self.giturl,
-                                                  self.src_path)) 
-        self.cmds.append('cd {}'.format(self.src_path))
-        #: dependancies
-        self.cmds.append('{} install -r requirements.txt'.format(self.pip_exe))
-        self.cmds.append('{} setup.py install'.format(self.python_exe))
-        self.cmds.append("fpm -s dir -t deb -n {} {}".format(
+                SubCmd(['mkdir', '-p', self.venv_path]),
+        
+                SubCmd(['mkdir', '-p', self.src_path]),
+                
+                SubCmd(['virtualenv', '-p', '/usr/bin/python',
+                                               self.venv_path]),
+                
+                #replace with with
+                #self.cmds.append('. {}/bin/activate'.format(self.venv_path))
+        
+                SubCmd(['git', 'clone', self.giturl, self.src_path]),
+                
+#                SubCmd(['cd', self.src_path]),
+                SubCmd([], os.chdir, [self.src_path]),
+                #: dependancies
+                SubCmd([self.pip_exe, 'install', '-r', 'requirements.txt']),
+                
+                SubCmd([self.python_exe, 'setup.py', 'install']),
+                SubCmd(['fpm', '-s', 'dir', '-t', 'deb', '-n',
                                                self.pkg_name,
-                                               self.venv_path
-                        ))
+                                               self.venv_path])
+        ):
+            self.cmds.append(cmd)
 
 d = Deployment('pyhello',
                'github:mikadosoftware/pyhelloworld.git')
 d.prepare_venv()
-pprint.pprint(d.cmds)
+import subprocess
+for cmd in d.cmds:
+    print cmd
+    print
+    raw_input("?")
+    if not cmd.args:
+        print subprocess.check_call(cmd.cmdlist)
+    else:
+        cmd.pythonstmt.__call__(*cmd.args)
+
 
